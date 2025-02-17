c65de16215f85ff302ab4bb8502b6b7d9256084c
Evanderson Ribeiro <evanderson.coder@gmail.com>
2025-02-17 14:21:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -42,6 +42,14 @@
         $stmt->execute();
         $result = $stmt->get_result();
 
+	// Exibe os estados dos LEDs
+        while ($row = $result->fetch_assoc()) {
+            echo "<p>LED " . substr($row["led_id"], 4) . ": <span id=\"" . $row["led_id"] . "\">" . htmlspecialchars($row["estado"]) . "</span></p>";
+            echo "<button class=\"button\" onclick=\"mudarEstado('" . $row["led_id"] . "', 'ON')\">Ligar " . substr($row["led_id"], 4) . "</button>";
+            echo "<button class=\"button\" onclick=\"mudarEstado('" . $row["led_id"] . "', 'OFF')\">Desligar " . substr($row["led_id"], 4) . "</button>";
+            echo "<br><br>";
+        }
+
         $stmt->close();
         $conn->close();
     
