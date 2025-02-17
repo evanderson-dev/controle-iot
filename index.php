710c981cb6e0419a797348249933d0e4675efaf7
evanderson <axel_@DESKTOP-92MEPRT>
2025-02-17 15:17:01 -0300

    Move mudarEstado function to script.js and update index.php to include the script

--- index.php
+++ index.php
@@ -44,27 +44,6 @@
     }
     ?>
 	
-    <script>
-		function mudarEstado(ledId, novoEstado) {
-			const params = "led_id=" + encodeURIComponent(ledId) + "&novo_estado=" + encodeURIComponent(novoEstado);
-
-			fetch('/atualizar_estado.php', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				},
-				body: params
-			})
-			.then(response => response.text())
-			.then(data => {
-				console.log(data); // Exibe a resposta do servidor no console
-				location.reload(); // Recarrega a página para exibir o novo estado
-			})
-			.catch(error => {
-				console.error('Erro:', error);
-				alert('Ocorreu um erro ao comunicar com o servidor.');
-			});
-		}
-	</script>
+	<script src="script.js"></script>
 </body>
 </html>
